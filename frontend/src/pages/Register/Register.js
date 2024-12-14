0f410a2e5aec61af0e7684ababa372c9164a4a9c
Maheshwar-Reddy-P <pmaheshwarreddy243@gmail.com>
2024-12-14 22:11:03 +0530

    Updated Login and Register Page

--- frontend/src/pages/Register/Register.js
+++ frontend/src/pages/Register/Register.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 import axios from "axios";
+import "./Register.css";
 const Register = () => {
   const [formData, setFormData] = useState({
     name: "",
@@ -116,72 +118,95 @@ const Register = () => {
 
   return (
     <div className="login-page">
-      <div className="login-card">
+      <div className="register-card">
         <form onSubmit={handleSubmit} className="login-form">
           <h2>Join the Pickle Party</h2>
           <p>Your Taste Buds are Invited!</p>
-          <div>
+          <div class="form-group">
             <input            
               type="text"
               name="name"
+              id="name"
+              placeholder=" "
               value={formData.name}
-              placeholder="Name"
               onChange={(e) => setFormData({ ...formData, name: e.target.value })}
               required
             />
+            <label for="name">Name</label>
             {errors.customerName && <p style={{ color: "red" }}>{errors.customerName}</p>}
           </div>
-          <div>
+          <div class="form-group">
             <input
               type="email"
               name="email"
+              id="email"
               value={formData.email}
               onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-              placeholder="Email"
+              placeholder=" "
               required
             />
+            <label for="email">Email</label>
             {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
           </div>
-          <div>
+          <div class="form-group">
             <input
               type="text"
               name="mobileNumber"
+              id="mobileNumber"
               value={formData.mobileNumber}
               onChange={(e) => setFormData({ ...formData, mobileNumber: e.target.value })}
-              placeholder="Mobile Number"
+              placeholder=" "
               required
             />
+            <label for="mobileNumber">Mobile Number</label>
             {errors.mobileNumber && <p style={{ color: "red" }}>{errors.mobileNumber}</p>}
           </div>
-          <div>
+          <div class="form-group"> 
             <input
               type="password"
               name="password"
+              id="password"
               value={formData.password}
               onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-              placeholder="Password"
+              placeholder=" "
               required
             />
+            <label for="password">Password</label>
             {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
           </div>
-          <div>
+          <div class="form-group">
             <input
               type="password"
               name="confirmPassword"
+              id="confirmPassword"
               value={formData.confirmPassword}
               onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
-              placeholder="Confirm Password"
+              placeholder=" "
               required
             />
+            <label for="confirmPassword">Confirm Password</label>
             {errors.confirmPassword && <p style={{ color: "red" }}>{errors.confirmPassword}</p>}
           </div>
           <div className="login-btn-container">
-            <button type="submit" className="login-button">Register</button>
             <button type="button" onClick={handleReset} className="login-button">
               Reset
             </button>
+            <button type="submit" className="login-button">Register</button>
           </div>
         </form>
+
+        <p className="register-footer">
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+        <p className="or-text">or</p>
+        <button className="google-btn">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
+            alt="Google Logo"
+            className="google-icon"
+          />
+          Continue with Google
+        </button>
       </div>
     </div>
   );
