e3be60ed6a23ad937e6849a7dcf4652cb434af1d
Maheshwar-Reddy-P <pmaheshwarreddy243@gmail.com>
2025-02-01 16:40:41 +0530

    Authorization in backend

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import {getUserProfile, updateProfile } from '../controller/user.controller.js';
+import authMiddleware from '../middlewares/Authorization.js';
 const router = express.Router();
 //get user profile
-router.get('/profile/:userId', getUserProfile);
+router.get('/profile/:userId',authMiddleware, getUserProfile);
 //update profile
-router.put('/updateUser/:userId', updateProfile);
+router.put('/updateUser/:userId',authMiddleware, updateProfile);
 export default router;
