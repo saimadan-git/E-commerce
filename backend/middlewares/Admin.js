ed3d0cd41ed8154c2a4817d2bc3a12fa51e5648d
Koundinya-Madiraju <koundinya1312@gmail.com>
2025-02-18 22:29:19 +0530

    JWTExpire

--- backend/middlewares/Admin.js
+++ backend/middlewares/Admin.js
@@ -1,12 +1,15 @@
 import jwt from "jsonwebtoken";
 import User from "../models/Register.js"; // Import User model
-
+import dotenv from "dotenv";
+dotenv.config();
  const verifyAdmin = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1]; // Get token from headers
+    console.log("The token is:",token);
     if (!token) return res.status(401).json({ message: "Access denied. No token provided." });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify JWT
+    console.log("MAHI IS SMART");
     const user = await User.findById(decoded.id);
 
     if (!user || user.role !== "admin") {
@@ -16,7 +19,7 @@ import User from "../models/Register.js"; // Import User model
     req.user = user; // Attach user data to request
     next(); // Continue to the next middleware
   } catch (error) {
-    res.status(401).json({ message: "Invalid token." });
+    res.status(401).json({ message: error.message });
   }
 };
 export default verifyAdmin;
