c02e3e12a7a6de927c8d329708d4e27de8015a98
Koundinya-Madiraju <koundinya1312@gmail.com>
2025-03-04 22:47:59 +0530

    GetCart

--- backend/controller/cart.controller.js
+++ backend/controller/cart.controller.js
@@ -99,7 +99,7 @@ export const getCart = async (req, res) => {
         };
 
         return res.status(200).json({
-            status: "error",
+            status: "success",
             message: "Cart retrieved successfully",
             data: formattedCart
         });
@@ -150,7 +150,7 @@ export const updateCart = async (req, res) => {
         cart.totalPrice = cart.cartItems.reduce((total, item) => total + item.price, 0);
         await cart.save();
         return res.status(200).json({
-            status: "Success",
+            status: "success",
             message: "Cart updated successfully",
             data: cart
         });
