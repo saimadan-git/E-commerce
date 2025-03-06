c4f4b78b780cf37999e5d0ac9b4ed643fa48c589
Maheshwar-Reddy-P <pmaheshwarreddy243@gmail.com>
2025-03-06 23:33:41 +0530

    Fixed Background Image fitting issue in Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 import './App.css';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Login from "./pages/Login";
+import Login from "./pages/Login/Login";
 import Register from "./pages/Register";
 import Shop from "./pages/Shop";
 import ForgetPassword from "./pages/ForgetPassword";
