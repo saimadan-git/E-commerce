0d40d976578f3e9bed68954d531612e9a33f4f3f
Maheshwar-Reddy-P <pmaheshwarreddy243@gmail.com>
2025-03-13 22:24:14 +0530

    Fixed the Page Refresh Issue

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,9 +1,14 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
+import Loader from "./Loader";
 
 const ProtectedRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <Loader />;
+  }
   
   return user ? children : <Navigate to="/login" />;
 };
