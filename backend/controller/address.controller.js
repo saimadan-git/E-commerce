aa128b666237dd2a2a172cabc001ea7685f24502
Koundinya-Madiraju <koundinya1312@gmail.com>
2025-03-15 16:38:53 +0530

    CRUDOperationsOnAddressess

--- backend/controller/address.controller.js
+++ backend/controller/address.controller.js
@@ -93,33 +93,65 @@ export const updateAddress = async (req, res) => {
                 message: "User not found",
             });
         }
-        const updatedAddress = {
-            name,
-            mobileNumber,
-            pincode,
-            area,
-            address,
-            city,
-            state,
-            landmark,
-            alternateMobile,
-            type,
-        }
-        const updatedUser = user.addressess.findOneAndUpdate(addressId, updatedAddress, { new: true });
-        if (!updatedUser) {
+        const addressOne = user.addressess.find((address) => address._id == addressId);
+        if (!addressOne) {
             return res.status(404).json({
                 status: "error",
                 message: "Address not found",
             });
         }
+        if (name||mobileNumber||pincode||area||address||city||state||landmark||alternateMobile||type) {
+            addressOne.name = name;
+            addressOne.mobileNumber = mobileNumber;
+            addressOne.pincode = pincode;
+            addressOne.area = area;
+            addressOne.address = address;
+            addressOne.city = city;
+            addressOne.state = state;
+            addressOne.landmark = landmark;
+            addressOne.alternateMobile = alternateMobile;
+            addressOne.type = type;
+        }
         await user.save();
         return res.status(200).json({
             status: "success",
             message: "Address updated successfully",
-            data: updatedUser,
+            data: addressOne,
         });
-    }
-    catch (err) {
+}catch (err) {
+    return res.status(500).json({
+        status: "error",
+        message: "Internal server error",
+        error: err.message,
+    });
+  }
+}
+//Delete Address by ID
+export const deleteAddress = async (req, res) => {
+      const { userId, addressId } = req.params;
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                status: "error",
+                message: "User not found",
+            });
+        }
+        const addressIndex = user.addressess.findIndex((address) => address._id == addressId);
+        if (addressIndex == -1) {
+            return res.status(404).json({
+                status: "error",
+                message: "Address not found",
+            });
+        }
+        user.addressess.splice(addressIndex, 1);
+        await user.save();
+        return res.status(200).json({
+            status: "success",
+            message: "Address deleted successfully",
+            data: user.addressess,
+        });
+    } catch (err) {
         return res.status(500).json({
             status: "error",
             message: "Internal server error",
