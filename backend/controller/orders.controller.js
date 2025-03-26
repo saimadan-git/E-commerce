f0c0168c7c92b2bc15c4cfcd320374ec4a1f8eca
Koundinya-Madiraju <koundinya1312@gmail.com>
2025-03-26 23:55:41 +0530

    Payment Verification

--- backend/controller/orders.controller.js
+++ backend/controller/orders.controller.js
@@ -45,9 +45,13 @@ export const createOrder= async (req, res) => {
 export const verifyPayment = async (req, res) => {
     const {razorpay_payment_id,razorpay_order_id,razorpay_signature} = req.body;
     try{
+        console.log("Razorpay Order ID:", razorpay_order_id);
+        console.log("Razorpay Payment ID:", razorpay_payment_id);
+        console.log("Razorpay Signature:", razorpay_signature);
         const hmac = crypto.createHmac('sha256',process.env.RAZORPAY_KEY_SECRET);
         hmac.update(`${razorpay_order_id}|${razorpay_payment_id}`);
         const generatedSignature = hmac.digest('hex');
+        console.log("Generated Signature:", generatedSignature);
 
         if(generatedSignature === razorpay_signature){
             const order = await Order.findOneAndUpdate(
--- backend/models/order.js
+++ backend/models/order.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import moment from 'moment-timezone';
 const orderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
   items: [
@@ -7,9 +8,11 @@ const orderSchema = new mongoose.Schema({
       quantity: { type: Number, required: true },
     }
   ],
+  orderId: { type: String, required: true },
   orderDate: { type: Date, default: Date.now },
-  deliveryDate: { type: Date, default: Date.now },
-  status: { type: String, default: 'Order Not Placed' },
+  deliveryDate: { type: Date, default: moment().tz("Asia/Kolkata").toDate() },
+  status: { type: String, default: 'Order Not Placed' ,
+  },
   paymentStatus: { type: String, default: 'Pending' },
   amount: { type: Number, required: true },
   paymentId: { type: String },
