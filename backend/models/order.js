4011c2c557440bc6de73d9d5136022aec21d05b9
Koundinya-Madiraju <koundinya1312@gmail.com>
2025-04-10 23:21:53 +0530

    Item name in create Order

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import moment from 'moment-timezone';
+import { type } from "os";
 const orderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
   items: [
@@ -13,12 +14,12 @@ const orderSchema = new mongoose.Schema({
   ],
   orderId: { type: String, required: true },
   orderDate: { type: Date, default: Date.now },
-  deliveryDate: { 
-    type: Date, 
-    default: () => moment().tz("Asia/Kolkata").toDate(),
-  },
-    status: { type: String, default: 'Order Not Placed' ,
-  },
+  // deliveryDate: { 
+  //   type: Date, 
+  //   default: () => moment().tz("Asia/Kolkata").toDate(),
+  // },
+  //   status: { type: String, default: 'Order Not Placed' ,
+  // },
   paymentStatus: { type: String, default: 'Pending' },
   amount: { type: Number, required: true },
   paymentId: { type: String },
@@ -30,6 +31,8 @@ const orderSchema = new mongoose.Schema({
     state: { type: String, required: true },
     pincode: { type: String, required: true },
     mobileNumber: { type: String, required: true },
+    type: { type: String, required: true },
+    customType:{ type: String, default: null },
   },
   });
 export default mongoose.model("Order", orderSchema);
